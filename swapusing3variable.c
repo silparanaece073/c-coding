a5d97e0a23d747527eb05d5ed7f0e8aaf32454e1
silparanaece073 <swaruppattanayek53@gmail.com>
2026-04-02 12:37:07 +0530

    Create swapusing3variable.c0

---
+++ swapusing3variable.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    c = a;
+    a = b;
+    b = c;
+
+    printf("After swapping: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
